f6df6dbdfd8eee893b5e6035c005b96254d0ffcf
IgorAraujoV <igoraraujodev@gmail.com>
2020-08-24 13:20:30 -0300

    Redirect to login when not found route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import UserDetails from './pages/UserDetails';
@@ -12,6 +12,9 @@ export default function Routes() {
         <Route exact path="/" component={Login} />
         <Route path="/home" component={Home} />
         <Route path="/details/:userId" component={UserDetails} />
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
